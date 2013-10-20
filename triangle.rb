d9c232b22afb38c79c81ee856545f3de5d946632
James Keane <mail@james-keane.com>
2013-10-20 20:14:49 +0100

    Triangle 2 and iteration finished

--- triangle.rb
+++ triangle.rb
@@ -14,6 +14,15 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  # Lets check the rules of a Triangle
+  #
+  # If any side is less than 0, it can't be a triangle
+  raise TriangleError if [a,b,c].min <= 0
+  
+  # The sum of two sides must always be greater than the third
+  x,y,z = [a,b,c].sort
+  raise TriangleError if x + y <= z
+  
   if (a==b) && (b==c) & (a==c)
     :equilateral
   elsif (a==b) || (b==c) || (a==c)
